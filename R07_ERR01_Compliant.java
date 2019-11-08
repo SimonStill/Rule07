1d6daa72489f9c3dc2b98927c577e41d9fa91c0d
SimonStill <57300255+SimonStill@users.noreply.github.com>
2019-11-07 19:21:31 -0500

    Add files via upload

---
+++ R07_ERR01_Compliant.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *  Rule07 - ERR01
+ *  Compliant
+ ******************************************************************************/
+
+class ExceptionExample {
+  public static void main(String[] args) {
+ 
+    File file = null;
+    try {
+      file = new File(System.getenv("APPDATA") +
+             args[0]).getCanonicalFile();
+      if (!file.getPath().startsWith("c:\\homepath")) {
+        System.out.println("Invalid file");
+        return;
+      }
+    } catch (IOException x) {
+      System.out.println("Invalid file");
+      return;
+    }
+ 
+    try {
+      FileInputStream fis = new FileInputStream(file);
+    } catch (FileNotFoundException x) {
+      System.out.println("Invalid file");
+      return;
+    }
+  }
+}
+
+
+
